fc2d5cb09b1ee107b813f2ae44919c7290c9f8b5
Alex Sobhani <q8i@ornl.gov>
2023-08-23 13:24:04 -0400

    demonstration of interrupt setting records to 0

--- bug_test.py
+++ bug_test.py
@@ -1,13 +1,35 @@
 import time
 
+def prime_list(n):
+	if n==1:
+		return [2]
+	if n==2:
+		return [2, 3]
+	L = prime_list(n-1)
+	p = L[-1] + 2
+	while True:
+		is_prime = True
+		for f in L:
+			if p%f==0:
+				is_prime = False
+				break
+		if is_prime:
+			return L + [p]
+		p+=2
+	
+
 count = 5
 def trigger(p):
 	#time.sleep(5)
 	global count
 	print("here "+p+"A")
+	prime_list(10)
 	pydev.iointr(p+"A", count)
 	pydev.iointr(p+"B", count)
 	pydev.iointr(p+"C", count)
 	pydev.iointr(p+"D", count)
 	print("interrupt: "+p+"A")
 	#count += 1
+
+if __name__=="__main__":
+	print(prime_list(5))
